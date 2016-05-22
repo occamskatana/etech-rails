e3efb3783109084203f3336b353eab566eb97c80
occamskatana <johngallweycarter@gmail.com>
2016-05-21 21:38:23 -0400

    fixed join table

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,10 +7,18 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
-admin = User.create!(email: 'johngallweycarter@gmail.com', password: 'helloworld')
+admin = User.create!(email: 'johngallweycarter@gmail.com', password: 'helloworld', role: 'case_manager')
 
 
-Resident.create(email: 'jgfc35@aol.com', password: 'helloworld', first_name: 'Ben', last_name: 'Simmons', phone_number: Faker::PhoneNumber.cell_phone, calendar: 'https://e-tech.firebaseio.com/users/bdsimmons/events', user: User.first)
+Resident.create(
+	email: 'jgfc35@aol.com', 
+	password: 'helloworld', 
+	first_name: 'Ben', 
+	last_name: 'Simmons', 
+	phone_number: Faker::PhoneNumber.cell_phone, 
+	calendar: 'https://e-tech.firebaseio.com/users/bdsimmons/events', 
+	user: User.first
+	)
 
 
 
