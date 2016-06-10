00b019ab41bd55d8b621b843addb619790ef2d4c
John Carter <johngallweycarter@gmail.com>
2016-06-10 14:31:54 -0400

    serializing locations from firebase

--- app/serializers/resident_serializer.rb
+++ app/serializers/resident_serializer.rb
@@ -2,6 +2,8 @@ class ResidentSerializer < ActiveModel::Serializer
   attributes :id, :first_name, :last_name, :phone_number, :calendar, :phase, :sober_date, :locations
 
   def locations
-  	return "https://evolutiontech.firebaseio.com/residents/#{object.id}/locations"
+  	firebase = Firebase::Client.new("https://evolutiontech.firebaseio.com/residents/#{object.id}/locations")
+  	response = firebase.get('')
+  	return [response.body]
   end
 end
