6c7daa6af5ebcc54dcbf5856af5c8fcca96a0bd7
John Carter <johngallweycarter@gmail.com>
2016-06-23 18:07:56 -0400

    changed url field to string

--- db/migrate/20160509025359_add_fields_to_residents.rb
+++ db/migrate/20160509025359_add_fields_to_residents.rb
@@ -3,7 +3,7 @@ class AddFieldsToResidents < ActiveRecord::Migration
     add_column :residents, :first_name, :string
     add_column :residents, :last_name, :string
     add_column :residents, :phone_number, :string
-    add_column :residents, :calendar, :url
+    add_column :residents, :calendar, :string
     add_column :residents, :sober_date, :string
   end
 end
