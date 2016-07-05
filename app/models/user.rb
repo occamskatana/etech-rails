bf04cafd5ab7d88db25a2801ea0839df6f7b1c45
john <johngallweycarter@gmail.com>
2016-07-05 17:42:15 -0400

    trying to fix devise - removed rememberable

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,7 +22,8 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :trackable, :validatable 
+         # , :rememberable
 
    has_many :cases
    has_many :residents, through: :cases
