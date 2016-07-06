d733b5aec034dfc2fd14f52acef792d994e55956
John Carter <johngallweycarter@gmail.com>
2016-07-06 15:22:11 -0400

    devise unfucked

--- app/controllers/user/sessions_controller.rb
+++ app/controllers/user/sessions_controller.rb
@@ -1,25 +1,36 @@
 class User::SessionsController < Devise::SessionsController
-# before_action :configure_sign_in_params, only: [:create]
+prepend_before_filter :require_no_authentication, :only => [:create ]
+  include Devise::Controllers::InternalHelpers
+  
+  before_filter :ensure_params_exist
 
-  # GET /resource/sign_in
-  # def new
-  #   super
-  # end
+  respond_to :json
+  
+  def create
+    build_resource
+    resource = User.find_for_database_authentication(:login=>params[:user_login][:login])
+    return invalid_login_attempt unless resource
 
-  # POST /resource/sign_in
-  # def create
-  #   super
-  # end
+    if resource.valid_password?(params[:user_login][:password])
+      sign_in("user", resource)
+      render :json=> {:success=>true, :auth_token=>resource.authentication_token, :login=>resource.login, :email=>resource.email}
+      return
+    end
+    invalid_login_attempt
+  end
+  
+  def destroy
+    sign_out(resource_name)
+  end
 
-  # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
+  protected
+  def ensure_params_exist
+    return unless params[:user_login].blank?
+    render :json=>{:success=>false, :message=>"missing user_login parameter"}, :status=>422
+  end
 
-  # protected
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
+  def invalid_login_attempt
+    warden.custom_failure!
+    render :json=> {:success=>false, :message=>"Error with your login or password"}, :status=>401
+  end
 end
