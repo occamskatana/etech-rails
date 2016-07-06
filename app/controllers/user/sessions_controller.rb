b7069832e8f6de3194ce78c7b4b327335386613a
John Carter <johngallweycarter@gmail.com>
2016-07-06 15:24:40 -0400

    trying again

--- app/controllers/user/sessions_controller.rb
+++ app/controllers/user/sessions_controller.rb
@@ -1,36 +1,13 @@
 class User::SessionsController < Devise::SessionsController
 prepend_before_filter :require_no_authentication, :only => [:create ]
-  include Devise::Controllers::InternalHelpers
-  
-  before_filter :ensure_params_exist
-
-  respond_to :json
+ respond_to :json, :html
   
   def create
-    build_resource
-    resource = User.find_for_database_authentication(:login=>params[:user_login][:login])
-    return invalid_login_attempt unless resource
-
-    if resource.valid_password?(params[:user_login][:password])
-      sign_in("user", resource)
-      render :json=> {:success=>true, :auth_token=>resource.authentication_token, :login=>resource.login, :email=>resource.email}
-      return
-    end
-    invalid_login_attempt
-  end
-  
-  def destroy
-    sign_out(resource_name)
-  end
-
-  protected
-  def ensure_params_exist
-    return unless params[:user_login].blank?
-    render :json=>{:success=>false, :message=>"missing user_login parameter"}, :status=>422
-  end
-
-  def invalid_login_attempt
-    warden.custom_failure!
-    render :json=> {:success=>false, :message=>"Error with your login or password"}, :status=>401
+    sign_out(:user) if current_user
+    self.resource = warden.authenticate!(auth_options)
+    set_flash_message(:notice, :signed_in) if is_flashing_format?
+    sign_in(resource_name, resource)
+    yield resource if block_given?
+      respond_with resource, location: after_sign_in_path_for(resource)
   end
 end
